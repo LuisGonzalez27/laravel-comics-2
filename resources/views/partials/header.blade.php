e653eca5c0730d0b60c8519f28c93df08bb8a4bb
LuisGonzalez27 <luis27coding@gmail.com>
2023-01-06 21:45:45 +0100

    Layout Header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,67 @@
-<header>
-    sono header
+<header class="my-container">
+    <a href="#">
+        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC Logo">
+    </a>
+    <nav>
+        <ul>
+            <li>
+                <a href="#">
+                    CHARACTERS
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    COMICS
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    MOVIES
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    TV
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    GAMES
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    COLLECTIBLES
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    VIDEOS
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    FANS
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    NEWS
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    SHOP
+                </a>
+            </li>
+        </ul>
+    </nav>
+
 </header>
+<div class="jumbotron">
+    <div class="my-container">
+        <div class="headline">
+            current series
+        </div>
+    </div>
+</div>
