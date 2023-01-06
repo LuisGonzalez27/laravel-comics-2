74cb12475292c39744ae6c8cbcdf0d2b8cd5023d
LuisGonzalez27 <luis27coding@gmail.com>
2023-01-06 18:51:21 +0100

    Operazioni con CRUD

---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('content')
+<section class="container">
+    <h1>{{$comic->title}}</h1>
+</section>
+    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Guest\ComicContoller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
+
+// Route::get('/comics', [ComicContoller::class, 'index'])->name('comics.index');
+
+Route::resource('comics', ComicContoller::class);
